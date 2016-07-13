bbcece6dfb4d8bc0b63e0ded70681d5b7268330f
James Hunt <james@niftylogic.com>
2016-07-13 00:25:06 -0400

    Simplify qname-string contract test runner
    It doesn't need to be concerned with memory management.  Also, if
    chomp() just returns its argument, it can be call-chained.

--- t/contract/r/qname-string.c
+++ t/contract/r/qname-string.c
@@ -3,24 +3,20 @@
 #include <string.h>
 #include <bolo.h>
 
-void chomp(char *s)
+char* chomp(char *s)
 {
 	char *nl = strrchr(s, '\n');
 	if (nl) {
 		*nl = '\0';
 	}
+	return s;
 }
 
 int main(int argc, char **argv)
 {
-	char *s, buf[8192];
+	char buf[8192];
 	while ( (fgets(buf, 8192, stdin)) != NULL ) {
-		chomp(buf);
-		bolo_qname_t qn = bolo_qname_parse(buf);
-		s = bolo_qname_string(qn);
-		printf("%s\n", s);
-		free(s);
-		bolo_qname_free(qn);
+		printf("%s\n", bolo_qname_string(bolo_qname_parse(chomp(buf))));
 	}
 	return 0;
 }
